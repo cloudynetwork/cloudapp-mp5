2b6c26e574004ce2afd68af4dea3b7fa0b798b49
Peter Joseph <peter@cloudynetwork.org>
2015-10-10 11:24:56 +0100

    Need to complete ClusterCars class.

--- src/KMeansMP.java
+++ src/KMeansMP.java
@@ -16,7 +16,56 @@ import scala.Tuple2;
 
 
 public final class KMeansMP {
-	// TODO
+	
+	private static class ParsePoint implements Function<String, Vector> {
+		
+		private static final Pattern spacechar = Pattern.compile(",");
+		
+		public Vector call(String line) {
+			String[] token = spacechar.split(line);
+			
+			double[] point = new double[token.length-1];
+			
+			for (int i = 1; i < token.length; ++i) {
+				
+				point[i-1] = Double.parseDouble(token[i]);
+			}
+			return Vectors.dense(point); 
+		}
+		
+	}
+	
+	private static class ParseTitle implements Function<String, String> {
+		
+		private static final Pattern spacechar = Pattern.compile(",");
+		
+		public String call(String line) {
+			String[] token = spacechar.split(line);
+			
+			return token[0];
+		}
+	}
+	
+	private static class PrintCluster implements VoidFunction<Tuple2<Integer, Iterable<String>>> {
+		private KMeansModel model;
+		
+		public PrintCluster (KMeansModel model) {
+			this.model = model;
+		}
+		
+		public void call(Tuple2<Integer, Iterable<String>> Cars) throws Exception {
+			String ret = "[";
+			
+			for(String car: Cars._2()) {
+				ret += car + ", ";
+			}
+			
+			System.out.println(ret + "]");
+		}
+	}
+	
+	
+	
     public static void main(String[] args) {
         if (args.length < 2) {
             System.err.println(
